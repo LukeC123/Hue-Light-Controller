028f28f8d246310d838f1d262bb26cc0b1f13a3c
Luke Cheskin <bigtrak03@gmail.com>
2021-09-11 22:33:11 +0100

    Add project files.

---
+++ Hue Light Controller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hue_Light_Controller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
